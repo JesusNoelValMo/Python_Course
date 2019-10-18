d9bb78490a7b766f50df3324af4201a45879c9d0
Jesus Noel Valdibiezo Mora <jesusnoel.valdibiezomora@outlook.com>
2019-10-18 00:34:22 -0600

    Adding some examples of variable types

--- First_Program/main.py
+++ First_Program/main.py
@@ -1,3 +1,8 @@
+number = 1
+string = "string"
+concatenateString = "string1" + 'string 2'
+true_var = 5 is 5
+new_arr = ["value 1", "Value 2", "Value 3"]
+new_Dic = {"index_1": "Value_1", "index_3": "Value_2", "index_3": "Value_3"}
 
-print("Hello Word")
-print("Hello Word v2.0")
+print(new_Dic["index_1"])
